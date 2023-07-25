c4d9e8cf28b83523a23a7f10356b19520bb7ca22
BabyDragon003 <golddragon01234@gmail.com>
2023-07-25 10:54:52 +0900

    "2023-07-25 10:54"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
+import { useMemo } from "react";
 import Box from "@mui/material/Box";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./Home";
-
-import { Wallets } from './components/wallet'
-
-import { SnackbarProvider } from 'notistack';
-
-function App() {
 
 // I will try to start with new project
 // this project is so stressful
