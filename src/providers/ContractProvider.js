9ae068b839679478c34c0c1c88fdc2822413cf04
BabyDragon003 <golddragon01234@gmail.com>
2023-07-27 10:54:52 +0900

    "2023-07-27 10:54"

--- src/providers/ContractProvider.js
+++ src/providers/ContractProvider.js
@@ -8,6 +8,17 @@ import {
 } from "../contracts/bean"
 
 export const ContractContext = createContext({
+  minersCount: 0,
+  beanRewards: 0,
+  walletSolBalance: 0,
+  contractSolBalance: 0,
+  toggleDataUpdate: () => {},
+});
+
+export const ContractProvider = ({ children }) => {
+  const [minersCount, setMinersCount] = useState(0);
+  const [beanRewards, setBeanRewards] = useState(0);
+  const [walletSolBalance, setWalletSolBalance] = useState("0");
   const [contractSolBalance, setContractSolBalance] = useState("0");
   const [dataUpdate, setDataUpdate] = useState(false);
 
