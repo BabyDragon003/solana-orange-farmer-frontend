10dafc92184f5d9673ff90545535b937353904da
BabyDragon003 <golddragon01234@gmail.com>
2023-09-21 10:54:52 +0900

    "2023-09-21 10:54"

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ import reportWebVitals from "./reportWebVitals";
 import ThemeProvider from "./Theme";
 
 import { ContractProvider } from "./providers/ContractProvider";
+
+ReactDOM.render(
+  <>
+    <CssBaseline />
+      <ThemeProvider>
+        <ContractProvider>
+          <App />
+        </ContractProvider>
+      </ThemeProvider>
+  </>,
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
